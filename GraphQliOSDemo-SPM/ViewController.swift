50ff0e229a144aae4208beaa7bc83bf7a95bf0b5
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-05-14 15:57:14 +0530

    Initial Commit

---
+++ GraphQliOSDemo-SPM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphQliOSDemo-SPM
+//
+//  Created by Manu Puthoor on 14/05/20.
+//  Copyright © 2020 Manu Puthoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
